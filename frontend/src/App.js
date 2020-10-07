2121005a4f5e933833c140d52c645e618b704ea8
ricky <rjbenavides@cpp.edu>
2020-10-07 11:52:08 -0700

    added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 const App = () => {
   return (
     <>
-      <Header />
-      <main className="py-3">
-        <Container>
-          <h1>Welcome To Proshop</h1>
-          <HomeScreen />
-        </Container>
-      </main>
-      <Footer />
+      <Router>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <h1>Welcome To Proshop</h1>
+            <Route exact path="/" component={HomeScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
+          </Container>
+        </main>
+        <Footer />
+      </Router>
     </>
   );
 };
