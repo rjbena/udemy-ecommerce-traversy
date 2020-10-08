38e665a1bc5ad6f23eceefcc4ce554fc0fc87f1d
ricky <rjbenavides@cpp.edu>
2020-10-08 14:57:42 -0700

    Added db Schemas

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import colors from "colors";
 import dotenv from "dotenv";
 import products from "./data/products.js";
 import connectDB from "./config/db.js";
@@ -26,5 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
