732a34a40429c73164615bc31504f7ba855fb856
ricky <rjbenavides@cpp.edu>
2020-10-09 15:29:01 -0700

    added redux store and product reducer

---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,18 @@
+import {
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+} from "../constants/productConstants";
+
+export const productListReducer = (state = { products: [] }, action) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true, products: [] };
+    case PRODUCT_LIST_SUCCESS:
+      return { loading: false, products: action.payload };
+    case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
