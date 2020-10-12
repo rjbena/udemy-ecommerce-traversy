169b17071ac363196eded1f5b0a04f6a5a66f257
ricky <rjbenavides@cpp.edu>
2020-10-12 11:19:15 -0700

    added login and jwt for server

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+} from "../controllers/userControllers.js";
+
+router.route("/").post(registerUser);
+router.post("/login", authUser);
+router.route("/profile").get(protect, getUserProfile);
+export default router;
