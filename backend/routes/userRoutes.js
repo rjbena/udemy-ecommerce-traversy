ea55baf713ee4f1d37cb209e43daa890e8fa6916
ricky <rjbenavides@cpp.edu>
2020-10-13 09:24:49 -0700

    Added backend for updating user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,9 +7,13 @@ import {
   authUser,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } from "../controllers/userControllers.js";
 
 router.route("/").post(registerUser);
 router.post("/login", authUser);
-router.route("/profile").get(protect, getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 export default router;
