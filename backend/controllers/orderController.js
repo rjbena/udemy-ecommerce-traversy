dba33a69ad0237d3ca185fab2e03e91bef68c5e6
ricky <rjbenavides@cpp.edu>
2020-10-14 16:18:46 -0700

    backend for paying order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -43,10 +43,11 @@ const addOrderItems = asyncHandler(async (req, res) => {
 // @ access Private
 
 const getOrderById = asyncHandler(async (req, res) => {
-  const order = await (await Order.findById(req.params.id)).populate(
+  const order = await Order.findById(req.params.id).populate(
     "user",
     "name email"
   );
+
   if (order) {
     res.json(order);
   } else {
@@ -55,4 +56,28 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById };
+// @desc Update Order to paid
+// @ route Get /api/orders/:id/pay
+// @ access Private
+
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid };
